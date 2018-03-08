e0c5901dd7393ca9c84eadfbcea7de1a6c3f0ddd
David Wallace <dwallace0723@gmail.com>
2018-03-08 14:56:21 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="pymode",
-        version="1.0.6a1",
+        version="1.0.7a1",
         description="Typed interactions with the Mode Analytics API",
         keywords=["mode analytics", "api"],
         classifiers=[
